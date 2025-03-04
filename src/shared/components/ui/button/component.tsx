62c2c2286b90112999749c969b51c8809009b1d7
Hfreitas <hfreitas86@gmail.com>
2025-03-04 14:08:26 -0300

    feat: updates button component structure

---
+++ src/shared/components/ui/button/component.tsx
@@ -0,0 +1,34 @@
+import { Slot } from "@radix-ui/react-slot";
+import * as React from "react";
+import { buttonVariants } from "./variants";
+import type { VariantProps } from "class-variance-authority";
+
+import { cn } from "@/shared/helpers/class-names";
+
+export type ButtonProps = React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  };
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...properties
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...properties}
+    />
+  );
+}
+
+export { Button };
